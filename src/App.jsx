19859a9297eac2d03a38adf9c082b3ded76cc778
ivan-dias-dev <mangakaivan@gmail.com>
2024-06-01 10:20:40 -0300

    Add fix for the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import {Post} from "./Components/Post"
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Post />
+      <Post />
+      <Post />
     </>
   )
 }
 
 export default App
+
+
+
---
+++ src/Components/Post.jsx
@@ -0,0 +1,16 @@
+export function Post() {
+    return (
+        <h1>Post</h1>
+    )
+}
+
+//default export vs name exports
+
+
+// function Post(){
+//     return (
+//         <h1>Post</h1>
+//     )
+// }
+
+// export default Post
