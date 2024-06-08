eac84e78d164d72a69e1b9ffe7388572fad31a21
ivan-dias-dev <mangakaivan@gmail.com>
2024-06-08 18:55:09 -0300

    finalizando sem o Ts

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -3,6 +3,9 @@ import styles from "./Comment.module.css"
 import { Avatar } from "./Avatar"
 
 export function Comment({ content }) {
+    function handleDeleteComment() {
+        console.log("Deletar", event.target)
+    }
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/105668055?v=4" alt="Ivan Dias" />
@@ -13,7 +16,7 @@ export function Comment({ content }) {
                             <strong>Ivan Dias</strong>
                             <time dateTime="11 de Maio às 08:13">Cerca de 1h atrás</time>
                         </div>
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
